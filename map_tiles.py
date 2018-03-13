bab114a52c17af9b8cf6cf63c3a22f59f305528c
Dominatoriv98 <37068649+Dominatoriv98@users.noreply.github.com>
2018-03-13 13:43:20 +0000

    map tiles version 1 (tiles dont have inventory)

---
+++ map_tiles.py
@@ -0,0 +1,39 @@
+import items, enemies, player, random
+class maptile:
+    def __init__(self, x, y):
+        self.x = x 
+        self.y = y
+    def room_text(self):
+        raise NotImplementedError()
+    def modify_player(self, player):
+        raise NotImplementedError()
+class Starting_room(maptile):
+    def room_text(self):
+        return ()
+    def modify_player(self, player):
+        pass
+class loot_room(maptile):
+    def __init__(self, x, y, item):
+        self.item = item
+        super().__init__(x, y)
+    def room_text(self):
+        retun ()
+    def modify_player(self, player):
+        self.player.invontory.apend(self.item)
+class Combat_room(maptile):
+    def __init__(self, x, y, enemy):
+        self.enemy = enemy 
+        super().__init__(x, y)
+    def modify_player(self, player, combat):
+        pass
+    class combat():
+        def attack(self, item, enemy):
+            enemy.hp = enemy.hp - random.randint(0, 1) * wepon.dmg
+        def block(self, item, enemy, player, random):
+            player.hp = player.hp - max(0, random.randint(0, 1) * enemy.dmg - item.block)
+        def player_death(self, player):
+            if player. hp <= 0:
+                print ()
+        def enemy_death(self, enemy):
+            if enemy.hp <= 0:
+                print ()
