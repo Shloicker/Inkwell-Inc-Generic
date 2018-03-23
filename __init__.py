22cebce46f9f1af22eee2d15c528a199d78583d2
Dylan Stocker <36962371+Shloicker@users.noreply.github.com>
2018-03-23 12:12:24 +0000

    Various changes related to actions
     Created the actions file. It defines generic items and contains a number of instances of actions. I have deviated from the tutorial by creating instances of actions rather than sub-classes as creating sub-classes seemed to be a needless complication at this stage (yes the __init__ is capable of importing instances). I added a do_action function to the player class. I also added an observe item function to the player class to integrate the observe item function with the actions file. All relevant functionalities have been tested (including adding extra arguments through **kwargs).

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 from .items import item, weapon, healing_consumable, armour, shield, currency
 from .player import player
 from .enemies import enemy
+from .actions import action, TakeInventory, EquipWeapon, ViewEquippedItems, MoveNorth, MoveSouth, MoveEast, MoveWest, Attack
