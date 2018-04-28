1121934a2f26591e85fa172db338cd878008dcce
Dylan Stocker <36962371+Shloicker@users.noreply.github.com>
2018-04-28 21:47:43 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,5 +2,5 @@ from .world import _world, tile_exists, _objects, object_exists, world_currency
 from .items import item, weapon, healing_consumable, armour, shield, currency
 from .enemies import enemy
 from .player import player
-from .actions import action, action_arg, TakeInventory, Equip, MoveNorth, MoveSouth, MoveEast, MoveWest, Attack, Observe, LookAround, PickUp, Drop, Buy, Sell, Consume
+from .actions import action, action_arg, TakeInventory, Equip, MoveNorth, MoveSouth, MoveEast, MoveWest, Attack, Observe, LookAround, PickUp, Drop, Buy, Sell, Consume, Flee
 from .map_tiles import map_tile, starting_room, loot_room, shop_room, combat_room, victory_room
