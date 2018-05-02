db63a37a780e4abe54fdc97c8245a22bec108707
Dylan Stocker <36962371+Shloicker@users.noreply.github.com>
2018-05-02 17:32:52 +0100

    Update game_enemies.py

--- game_enemies.py
+++ game_enemies.py
@@ -1,4 +1,4 @@
 from text_adventure import enemy
 
-giant_spider = enemy("Giant Spider", "A venemous spider is large as a bear.", 200, 25, 15, 15)
+giant_spider = enemy("Giant Spider", "A venemous spider is large as a bear.", 50, 25, 15, 15)
 skeleton = enemy("Skeleton", "An undead decayed to bare bones somehow standing wields a rusted blade.", 100, 35, 60, 35)
