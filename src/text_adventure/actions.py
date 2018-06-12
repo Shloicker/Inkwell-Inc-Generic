a612f015e48a5682974e6b921dc3b52692d6e444
Dylan Stocker <36962371+Shloicker@users.noreply.github.com>
2018-06-12 17:23:40 +0100

    Add files via upload

--- src/text_adventure/actions.py
+++ src/text_adventure/actions.py
@@ -1,44 +1,42 @@
 from text_adventure.player import player
 
 class action(object):
-    def __init__(self, method, name):
+    def __init__(self, method, strings):
         self.method = method
-        self.name = name
+        self.strings = strings
 
     def __str__(self):
-        return "{}".format(self.name)
+        return self.strings[0]
 
 class action_arg(action):
-    def __init__(self, method, name, prompt):
-        self.prompt = prompt
-        super(action_arg, self).__init__(method, name)
+    pass
 
-MoveNorth = action(player.move_north, "move north")
+MoveNorth = action(player.move_north, ["move north", "north"])
 
-MoveSouth = action(player.move_south, "move south")
+MoveSouth = action(player.move_south, ["move south", "south"])
 
-MoveEast = action(player.move_east, "move east")
+MoveEast = action(player.move_east, ["move east", "east"])
 
-MoveWest = action(player.move_west, "move west")
+MoveWest = action(player.move_west, ["move west", "west"])
 
-TakeInventory = action(player.take_inventory, "take inventory")
+TakeInventory = action(player.take_inventory, ["take inventory", "inventory"])
 
-Attack = action(player.attack, "attack")
+Attack = action(player.attack, ["attack"])
 
-Equip = action_arg(player.equip, "equip", "What do you want to equip?")
+Equip = action_arg(player.equip, ["equip", "put on"])
 
-Consume = action_arg(player.consume, "consume", "What do you want to consume?")
+Consume = action_arg(player.consume, ["consume", "use"])
 
-Observe = action_arg(player.observe, "observe", "What do you want to observe?")
+Observe = action_arg(player.observe, ["observe", "look at", "stats"])
 
-LookAround = action(player.look_around, "look around")
+LookAround = action(player.look_around, ["look around", "search", "stock", "sale", "browse"])
 
-Drop = action_arg(player.drop, "drop", "What do you want to drop?")
+Drop = action_arg(player.drop, ["drop"])
 
-PickUp = action_arg(player.pick_up, "pick up", "What do you want to pick up?")
+PickUp = action_arg(player.pick_up, ["pick up", "retrieve", "retreive", "take"])
 
-Buy = action_arg(player.buy, "buy", "What do you want to buy?")
+Buy = action_arg(player.buy, ["buy", "purchase"])
 
-Sell = action_arg(player.sell, "sell", "What do you want to sell?")
+Sell = action_arg(player.sell, ["sell"])
 
-Flee = action(player.flee, "flee")
+Flee = action(player.flee, ["flee", "run away", "retreat"])
